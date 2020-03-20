7382357e17ac7ecf8c44ed0e914012ec2b660a61
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-03-20 13:33:34 -0400

    feat: Add exception stub for unknown requests
    Add `UnknownRequestException` to handle cases when the request is not
    supported.

---
+++ src/com/logmein/interview/UnknownRequestException.java
@@ -0,0 +1,14 @@
+package com.logmein.interview;
+
+public class UnknownRequestException extends Exception {
+
+	public UnknownRequestException(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3913984055154310595L;
+
+}
