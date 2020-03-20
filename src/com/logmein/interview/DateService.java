55f34979ec6444e92ba08155403138453030c889
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-03-20 14:58:41 -0400

    feat: Add javadoc for `DateService` and `DateTimeService`
    Add javadoc comment tags for `DateService` and `DateTimeService`.
    Add generated javadoc.

--- src/com/logmein/interview/DateService.java
+++ src/com/logmein/interview/DateService.java
@@ -1,5 +1,18 @@
+/*
+ * @author Maxime 'biximilien' Gauthier 
+ */
 package com.logmein.interview;
 
+/**
+ * The Interface DateService is an abstraction of the service handling the date
+ * request and returns an ISO 8601 compliant date.
+ */
 public interface DateService {
+
+	/**
+	 * Date request service handles the 'date' request.
+	 *
+	 * @return the date as an ISO 8601 compliant text string
+	 */
 	public String dateRequest();
 }
--- src/com/logmein/interview/DateTimeService.java
+++ src/com/logmein/interview/DateTimeService.java
@@ -1,5 +1,18 @@
+/*
+ * @author Maxime 'biximilien' Gauthier 
+ */
 package com.logmein.interview;
 
+/**
+ * The Interface DateTimeService is an abstraction of the service handling the date-time
+ * request and returns an ISO 8601 compliant date-time.
+ */
 public interface DateTimeService {
+	
+	/**
+	 * Date Time request service handles the 'datetime' request.
+	 *
+	 * @return the date-time as an ISO 8601 compliant text string
+	 */
 	public String dateTimeRequest();
 }
