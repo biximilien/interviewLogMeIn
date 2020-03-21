4ab4b990a98aaec4a9ea94c85abe6596afb95d40
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2020-03-21 12:04:05 -0400

    feat: Add support for multiple connections
    Refactor TCPTimeServer socket logic in a thread to ensure support for
    multiple concurrent connections.

--- src/com/logmein/interview/TCPTimeServer.java
+++ src/com/logmein/interview/TCPTimeServer.java
@@ -1,16 +1,13 @@
 package com.logmein.interview;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class TCPTimeServer extends AbstractTimeServer implements Runnable {
-	
+
 	private static final Logger LOGGER = Logger.getLogger(TCPTimeServer.class.getName());
 
 	private ServerSocket socket;
@@ -23,62 +20,32 @@ public class TCPTimeServer extends AbstractTimeServer implements Runnable {
 	@Override
 	public void run() {
 		running = true;
-		
+
 		while (running) {
 			Socket connectionSocket = null;
 
 			try {
 				connectionSocket = socket.accept();
-				LOGGER.log(Level.INFO, "Time Server accepted a connection on address " + socket.getInetAddress() + " and port " + socket.getLocalPort());
+				LOGGER.log(Level.INFO, "Time Server accepted a connection on address " + socket.getInetAddress()
+						+ " and port " + socket.getLocalPort());
 			} catch (IOException e) {
 				continue;
 			}
 
-			while (connectionSocket.isConnected() && !connectionSocket.isClosed()) {
-				BufferedReader inFromClient = null;
-				try {
-					inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-				} catch (IOException e3) {
-					break;
-				}
-
-				DataOutputStream outToClient = null;
-				try {
-					outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-				} catch (IOException e3) {
-					break;
-				}
-
-				String request = "";
-				try {
-					request = inFromClient.readLine();
-				} catch (IOException e2) {
-					e2.printStackTrace();
-				}
-				if (request != null) {
-					request.trim();
-				} else {
-					break;
-				}
-				LOGGER.log(Level.INFO, "Received TCP request `" + request + "`");
-
-				String response;
-				try {
-					response = requestHandler(request);
-				} catch (UnknownRequestException e1) {
-					response = "unknown request";
-				}
-				LOGGER.log(Level.INFO, "Sending TCP response `" + response + "`");
-				
-				response = response + "\n";
-
-				try {
-					outToClient.writeBytes(response);
-				} catch (IOException e) {
-					break;
-				}
-			}
+			TCPTimeServerThread threadedSocket = new TCPTimeServerThread(connectionSocket, this);
+			threadedSocket.start();
 		}
+
+		// close the socket if server is stopped
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void stop() {
+		running = false;
 	}
 
 }
---
+++ src/com/logmein/interview/TCPTimeServerThread.java
@@ -0,0 +1,75 @@
+package com.logmein.interview;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TCPTimeServerThread extends Thread {
+
+	private static final Logger LOGGER = Logger.getLogger(TCPTimeServerThread.class.getName());
+
+	private Socket socket;
+	private TCPTimeServer server;
+
+	public TCPTimeServerThread(Socket socket, TCPTimeServer server) {
+		this.socket = socket;
+		this.server = server;
+	}
+
+	@Override
+	public void run() {
+		while (socket.isConnected() && !socket.isClosed()) {
+			// setup input stream
+			BufferedReader inFromClient = null;
+			try {
+				inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			} catch (IOException e3) {
+				break;
+			}
+
+			// setup output stream
+			DataOutputStream outToClient = null;
+			try {
+				outToClient = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e3) {
+				break;
+			}
+
+			// read request from client
+			String request = "";
+			try {
+				request = inFromClient.readLine();
+			} catch (IOException e2) {
+				e2.printStackTrace();
+			}
+			if (request != null) {
+				request.trim();
+			} else {
+				break;
+			}
+			LOGGER.log(Level.INFO, "Received TCP request `" + request + "`");
+
+			// prepare response
+			String response;
+			try {
+				response = server.requestHandler(request);
+			} catch (UnknownRequestException e1) {
+				response = "unknown request";
+			}
+			LOGGER.log(Level.INFO, "Sending TCP response `" + response + "`");
+
+			response = response + "\n";
+
+			// send response to client
+			try {
+				outToClient.writeBytes(response);
+			} catch (IOException e) {
+				break;
+			}
+		}
+	}
+}
